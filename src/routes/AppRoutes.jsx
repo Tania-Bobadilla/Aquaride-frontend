040f885a2b3ce28bdefae67564abfa3303274657
FerRamirez99 <135175802+FerRamirez99@users.noreply.github.com>
2023-12-12 17:25:09 -0300

    Configuracion rutas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,12 +3,22 @@ import Layout from "../components/Layout"
 import Home from "../pages/Home"
 import ErrorPage from "../pages/ErrorPage"
 
+import Login from "../pages/Login"
+import Register from "../pages/Register"
+import Profile from "../pages/Profile"
+
 const AppRoutes = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/profile" element={<Profile />} />
+                    {/* <Route path="/products" element={<ProductList />} />
+                    <Route path="/products/:productID" element={<ProductItem />} />
+                    <Route path="/checkout" element={<Checkout />} /> */}
                     <Route path="*" element={<ErrorPage />} />
                 </Route>
             </Routes>
